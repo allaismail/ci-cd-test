c63cfc0c0a6c2c05c28cd9ffd50e934f884506ce
allaismail <wwwallaismail@gmail.com>
2024-03-27 14:33:06 +0100

    second testing ci-cd

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+# test_hello.py
+import unittest
+from app import add, subtract
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(1, 3), -2)
+
+if __name__ == '__main__':
+    unittest.main()
